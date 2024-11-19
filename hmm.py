a72159e67f9b24c79ec17f4a4c5f0608d4fa02e8
ted-yszhao <zys.theodore@gmail.com>
2024-11-19 09:48:45 +0000

    padding data and masking

--- hmm.py
+++ hmm.py
@@ -20,17 +20,18 @@ class HMM(torch.nn.Module ):
         self.n_features = n_features
 
         # Transition model in log space
-        self.transition_model = torch.nn.Parameter(torch.rand(n_states, n_states))
+        self.transition_model = torch.rand(n_states, n_states)
         
         # Emission model
         self.emission_model = EmissionModel(n_states,n_features)
 
         # Initial state
-        self.initial_state = torch.nn.Parameter(torch.rand(n_states))
+        self.initial_state = torch.rand(n_states)
 
         
         self.cuda_available = torch.cuda.is_available()
         if self.cuda_available:
+            print('Cuda Available.')
             self.device = torch.device('cuda')
             self.cuda()  
 
@@ -39,44 +40,75 @@ class HMM(torch.nn.Module ):
         """
             Encode the data using librosa
             params:
-                data: list of one audio file as numpy arrays
+                data: list of audio file as a list of numpy arrays, data[i] is the i-th audio file
+                        of shape (n_samples,)
+
                 sr: sample rate of the audio files, detault 16000
                 frame_length: length of the frame in milliseconds  
                 hop_length: hop length in milliseconds
 
             returns:
-                encoded_data: list of encoded data as T x n_features matrix
+                encoded_data: padded Tensor of shape (n_samples, max_length, n_features)
+                mask : Tensor of shape (n_samples, max_length)
                 tau : length of the frame in samples
         """
         n_fft = int(sr * frame_length / 1000)
         hop_length = int(sr * hop_length / 1000)
 
-        encoded_data = librosa.feature.mfcc(y=data, sr=sr,n_fft=n_fft,hop_length=hop_length,n_mfcc=self.n_features) 
+        encoded_data = []
+        for i in range(len(data)):
+            data[i] = data[i].astype(np.float32)
+            # normalize the data
+            data[i] = data[i] / np.max(np.abs(data[i]))
+            encoded_data.append(librosa.feature.mfcc(y=data[i], sr=sr,n_fft=n_fft,hop_length=hop_length,n_mfcc=self.n_features).T) 
 
         tau = n_fft
-        
-        return encoded_data.T , tau
 
+        encoded_data , mask = self.pad_data(encoded_data)
+        
+        return encoded_data , mask , tau
+
+    def pad_data(self,data):
+        """
+            Pad the data with -Inf to make all the data of the same length
+            params:
+                data: list of encoded data as a list of T x n_features matrix
+            returns:   
+                padded_data: Tensor of shape (n_samples, max_length, n_features)
+                mask: Tensor of shape (n_samples, max_length)
+        """
+        n_samples = len(data)
+        max_length = max([x.shape[0] for x in data])
+
+        padded_data = torch.full((n_samples,max_length,self.n_features),-np.inf)
+
+        for i in range(n_samples):
+            padded_data[i,:data[i].shape[0],:] = torch.tensor(data[i])
+
+        mask = padded_data[:,:,0] != -np.inf
+        return padded_data , mask
+    
     
     def train(self, data, n_iter = 100):
         """
             Train the HMM model using Expectation Maximization
             i.e. the Baum-Welch algorithm.
             params:
-                data: audio files as numpy array
+                data: list of audio files as numpy array
                 n_iter: number of EM iterations for training
         """
 
         # Perform MFCC encoding
-        data , tau = self.encode(data)
+        data , mask, tau = self.encode(data)
 
-        T , n_features_data = data.shape
+        N , _ , n_features_data = data.shape
 
         assert n_features_data == self.n_features
 
         # Send data to GPU if available
         if self.cuda_available:
             data = torch.tensor(data).to(self.device)
+            mask = torch.tensor(mask).to(self.device)
 
         # Initialize the model
         self.initialize_initial_state()
@@ -89,7 +121,7 @@ class HMM(torch.nn.Module ):
         
         
         # TODO: Implement the Baum-Welch algorithm
-        for i in tqdm(range(n_iter)):
+        for i in tqdm.tqdm(range(n_iter)):
             for x in data:
                 pass
     
@@ -119,7 +151,7 @@ class HMM(torch.nn.Module ):
         # self-transition probability 
         # strict left to right model
         for i in range(1,self.n_states-1):
-            self.transition_model[i,i] = -1 / (tau - 1)
+            self.transition_model[i,i] = np.exp(-1 / (tau - 1))
             self.transition_model[i,i+1] = 1 - self.transition_model[i,i]
         
         # absorbing terminal state
@@ -133,16 +165,15 @@ class HMM(torch.nn.Module ):
             Initialize the emission model
 
         """
-        global_mu = torch.mean(data,dim=0)
-        global_sigma = torch.cov(data.T)
+        global_mu = torch.mean(data,dim=0).to(self.device)
+        global_sigma = torch.cov(data.T).to(self.device)
 
         # Only using the diagonal elements of the covariance matrix
-        global_sigma = global_sigma  * torch.eye(self.n_features)
+        global_sigma = global_sigma  * torch.eye(self.n_features).to(self.device)
 
-        # Not using the first state and the last gaussian for the emission model
         for i in range(self.n_states):
             self.emission_model.gaussians[i].loc = global_mu
-            self.emission_model.gaussians[i].scale = global_sigma
+            self.emission_model.gaussians[i].covariance_matrix = global_sigma
 
     def hmm_forward(self,data):
         """
@@ -175,7 +206,7 @@ class EmissionModel(torch.nn.Module):
         self.n_states = n_states
         self.n_features = n_features
 
-        self.gaussians = [torch.distributions.MultivariateNormal(torch.rand(n_features),torch.rand(n_features,n_features)) for i in range(n_states)]
+        self.gaussians = [torch.distributions.MultivariateNormal(torch.eye(n_features),torch.eye(n_features,n_features)) for i in range(n_states)]
 
     def log_prob(self,state,data):
         """
